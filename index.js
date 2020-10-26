c02ba1ae9a8a13991aa165f2fd9f8d0dd33a1557
Alison Lubar <aslubar@gmail.com>
2020-10-25 23:54:26 -0400

    added prompts and messages to index

--- index.js
+++ index.js
@@ -5,7 +5,63 @@ const inquirer = require("inquirer")
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Enter your project title.",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter your project description.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps to install your product? Provide instructions here.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide instructions for use.",
+    },
+    {
+        type: "input",
+        name: "screenshot",
+        message: "Add the URL to a screenshot.",
+    },
 
+    {
+        type: "list",
+        message: "Please choose your license.",
+        name: "license",
+        choices: [
+            "MIT",
+            "other",
+            "another",
+            "last one"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please add any contributions.",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please enter any test instructions here.",
+    },
+    {
+        type: "input",
+        name: "gitHub",
+        message: "Please add the link to your GitHub profile. This will be added under the 'Questions' section.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please add your email address. This will be added under the 'Questions' section.",
+    },
 
 ];
 
