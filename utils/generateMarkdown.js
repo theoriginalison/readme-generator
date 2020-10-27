4e865db2744c1f7d68b4275aa4decce99aa333bf
Alison Lubar <aslubar@gmail.com>
2020-10-26 23:42:38 -0400

    created markdown template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,41 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+// need to add badges here!
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Screenshot](#screenshot)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Screeshot
+  ![Screenshot of ${data.title}](${data.screenshot})
+
+  ## License
+  ${data.license}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  Find me on GitHub here: ${data.gitHub}
+  Or you can reach me at ${data.email} for any additional questions.
+
 `;
 }
 
