5e533ccc5cad4e7257fa41eb91f5cd31c214e83c
Alison Lubar <aslubar@gmail.com>
2020-10-29 21:04:18 -0400

    cleaned up index JS file

--- index.js
+++ index.js
@@ -69,13 +69,6 @@ const questions = [
 
 ];
 
-
-// function to write README file
-// function writeToFile(fileName, data) {
-// }
-
-
-
 // function to initialize program
 function init() {
     inquirer.prompt(questions).then(answers => {
